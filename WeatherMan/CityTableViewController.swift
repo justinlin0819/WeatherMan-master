4f16e218ab690ec6748d852ddd11fc9c25b1ea54
Your Name <you@example.com>
2018-08-26 16:03:30 +0800

    加上App icon。

--- WeatherMan/CityTableViewController.swift
+++ WeatherMan/CityTableViewController.swift
@@ -10,97 +10,6 @@ import UIKit
 
 class CityTableViewController: UITableViewController {
     
-    @IBOutlet var button_city: [UIButton]!
-    /*var date = [String]()
-    var weather = [String]()
-    var city: String = ""*/
-    
-    @IBAction func button_cityselection(_ sender: UIButton) {
-        var date = [String]()
-        var weather = [String]()
-        var city: String = ""
-        switch sender {
-        case button_city[0]:
-            city = "臺北市"
-        case button_city[1]:
-            city = "新北市"
-        case button_city[2]:
-            city = "桃園市"
-        case button_city[3]:
-            city = "臺中市"
-        case button_city[4]:
-            city = "臺南市"
-        case button_city[5]:
-            city = "高雄市"
-        case button_city[6]:
-            city = "基隆市"
-        case button_city[7]:
-            city = "新竹縣"
-        case button_city[8]:
-            city = "新竹市"
-        case button_city[9]:
-            city = "苗栗縣"
-        case button_city[10]:
-            city = "彰化縣"
-        case button_city[11]:
-            city = "南投縣"
-        case button_city[12]:
-            city = "雲林縣"
-        case button_city[13]:
-            city = "嘉義縣"
-        case button_city[14]:
-            city = "嘉義市"
-        case button_city[15]:
-            city = "屏東縣"
-        case button_city[16]:
-            city = "宜蘭縣"
-        case button_city[17]:
-            city = "花蓮縣"
-        case button_city[18]:
-            city = "臺東縣"
-        case button_city[19]:
-            city = "澎湖縣"
-        case button_city[20]:
-            city = "金門縣"
-        case button_city[21]:
-            city = "連江縣"
-        default:
-            break
-        }
-        let urlStr = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=e6831708-02b4-4ef8-98fa-4b4ce53459d9"
-        if let url = URL(string: urlStr) {
-            let task = URLSession.shared.dataTask(with: url) { (data, response , error) in
-                if let data = data, let dic = try?
-                    
-                    JSONSerialization.jsonObject(with: data) as? [String: Any], let result1 = dic?["result"] as? [String: Any], let result2 = result1["results"] as? [[String: Any]]{
-                    for location in result2 {
-                        let id = location["_id"] as! String
-                        let city_t = location["locationName"] as! String
-                        let weather_t = location["parameterName1"] as! String
-                        let date_t = location["startTime"] as! String
-                        if (Int(id)! % 2 == 0) && city_t == city {
-                        date.append(String(date_t.prefix(10)))
-                        weather.append(String(weather_t))
-                    }
-                }
-              }
-            }
-            task.resume()
-        }
-        
-        while date.count != 7 {
-            
-        }
-         
-        if let controller = storyboard?.instantiateViewController(withIdentifier: "weathertableview") as? WeatherTableViewController {
-            for i in 0...6 {
-                controller.date.append(date[i])
-                controller.weather.append(weather[i])
-            }
-            present(controller, animated: true, completion: nil)
-        }
-   }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -114,7 +23,63 @@ class CityTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        var city: String = ""
+        switch indexPath.row {
+        case 1:
+            city = "臺北市"
+        case 2:
+            city = "新北市"
+        case 3:
+            city = "桃園市"
+        case 4:
+            city = "臺中市"
+        case 5:
+            city = "臺南市"
+        case 6:
+            city = "高雄市"
+        case 7:
+            city = "基隆市"
+        case 8:
+            city = "新竹縣"
+        case 9:
+            city = "新竹市"
+        case 10:
+            city = "苗栗縣"
+        case 11:
+            city = "彰化縣"
+        case 12:
+            city = "南投縣"
+        case 13:
+            city = "雲林縣"
+        case 14:
+            city = "嘉義縣"
+        case 15:
+            city = "嘉義市"
+        case 16:
+            city = "屏東縣"
+        case 17:
+            city = "宜蘭縣"
+        case 18:
+            city = "花蓮縣"
+        case 19:
+            city = "臺東縣"
+        case 20:
+            city = "澎湖縣"
+        case 21:
+            city = "金門縣"
+        case 22:
+            city = "連江縣"
+        default:
+            break
+        }
 
+        if let controller = storyboard?.instantiateViewController(withIdentifier: "weathertableview") as? WeatherTableViewController {
+            controller.city = city
+            present(controller, animated: true, completion: nil)
+        }
+    }
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
